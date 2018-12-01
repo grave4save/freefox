fffa2df96b331b54792671d86897760d3bc2e6a1
bogachenko <bogachenkove@gmail.com>
2018-12-02 00:38:33 +1100

    Created

---
+++ waterfox/user.js
@@ -0,0 +1,49 @@
+/*
+ * This file is part of the MozBackup project,
+ * Copyright (c) 2018 Bogachenko Vyacheslav
+ *
+ * MozBackup is a free project: you can distribute it and/or modify
+ * it in accordance with the MIT license published by the Massachusetts Institute of Technology.
+ *
+ * The MozBackup project is distributed in the hope that it will be useful,
+ * and is provided "AS IS", WITHOUT ANY WARRANTY, EXPRESSLY EXPRESSED OR IMPLIED.
+ * WE ARE NOT RESPONSIBLE FOR ANY DAMAGES DUE TO THE USE OF THIS PROJECT OR ITS PARTS.
+ * For more information, see the MIT license.
+ *
+ * Author: Bogachenko Vyacheslav <https://github.com/bogachenko>
+ * Email: bogachenkove@gmail.com
+ * Github: https://github.com/bogachenko/mozbackup/tree/master/waterfox/
+ * Last modified: December 2, 2018
+ * License: MIT <https://github.com/bogachenko/mozbackup/blob/master/LICENSE.md>
+ * Problem reports: https://github.com/bogachenko/mozbackup/issues
+ * Title: user.js
+ * URL: https://github.com/bogachenko/mozbackup/blob/master/waterfox/user.js
+ * Wiki: https://github.com/bogachenko/mozbackup/wiki
+ *
+ * Download the entire MozBackup project at https://github.com/bogachenko/mozbackup/archive/master.zip */
+ 
+ /* Warning in "about:config"
+ * Предупреждение в "about:config" */
+user_pref("general.warnOnAboutConfig", false);
+
+/* Warning in "about:networking"
+ * Предупреждение в "about:networking" */
+user_pref("network.warnOnAboutNetworking", false);
+
+/* Warning about closing multiple tabs
+ * Предупреждение о закрытии нескольких вкладок */
+user_pref("browser.tabs.warnOnClose", true);
+user_pref("browser.tabs.warnOnCloseOtherTabs", true);
+
+/* Warning about opening a large number of tabs
+ * Предупреждение о открытии большого количества вкладок */
+user_pref("browser.tabs.warnOnOpen", true);
+
+/* UserAgent */
+user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0");
+user_pref("general.buildID.override", "20100101");
+user_pref("general.appname.override", "Netscape");
+user_pref("general.appversion.override", "5.0 (Windows)");
+user_pref("general.platform.override", "Win32");
+user_pref("general.oscpu.override", "Windows NT 6.1");
+// user_pref("general.useragent.locale", "en-US");
