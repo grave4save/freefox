c70e9fdb33773d729219ba9e96d383182fafedf0
bogachenko <bogachenkove@gmail.com>
2018-12-13 10:35:05 +0000

    Created update.sh (#4)

---
+++ scripts/update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/bogachenko/libertyfox/master/fennec/user.js -O ../fennec/user.js
+wget https://raw.githubusercontent.com/bogachenko/libertyfox/master/firefox/user.js -O ../firefox/user.js
+wget https://raw.githubusercontent.com/bogachenko/libertyfox/master/firefox/chrome/userChrome.css -O ../firefox/chrome/userChrome.css
+wget https://raw.githubusercontent.com/bogachenko/libertyfox/master/palemoon/user.js -O ../palemoon/user.js
+wget https://raw.githubusercontent.com/bogachenko/libertyfox/master/palemoon/chrome/userChrome.css -O ../palemoon/chrome/userChrome.css
+wget https://raw.githubusercontent.com/bogachenko/libertyfox/master/thunderbird/user.js -O ../thunderbird/user.js
+wget https://raw.githubusercontent.com/bogachenko/libertyfox/master/thunderbird/chrome/userChrome.css -O ../thunderbird/chrome/userChrome.css
+wget https://raw.githubusercontent.com/bogachenko/libertyfox/master/waterfox/user.js -O ../waterfox/user.js
+wget https://raw.githubusercontent.com/bogachenko/libertyfox/master/waterfox/chrome/userChrome.css -O ../waterfox/chrome/userChrome.css
