acd4e82973dcb9d8c44d49f3b73f4c758a54287f
bogachenko <bogachenkove@gmail.com>
2019-01-17 13:04:10 +0000

    Update verify-userjs.sh

--- scripts/verify-userjs.sh
+++ scripts/verify-userjs.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
+clear
+
+echo "\033[37;1;41m User.js \033[0m"
 grep -E -n -v '^[ 	]*(//|#[a-z]+|(user_pref\("[a-zA-Z0-9._-]+", (true|false|-?[0-9]+|"([^\"]|\\")*")\);)|$)' user.js
+
+echo "\033[37;1;41m Fennec \033[0m"
+grep -E -n -v '^[ 	]*(//|#[a-z]+|(user_pref\("[a-zA-Z0-9._-]+", (true|false|-?[0-9]+|"([^\"]|\\")*")\);)|$)' fennec/user.js
+
+echo "\033[37;1;41m Firefox \033[0m"
+grep -E -n -v '^[ 	]*(//|#[a-z]+|(user_pref\("[a-zA-Z0-9._-]+", (true|false|-?[0-9]+|"([^\"]|\\")*")\);)|$)' firefox/user.js
+
+echo "\033[37;1;41m Palemoon \033[0m"
+grep -E -n -v '^[ 	]*(//|#[a-z]+|(user_pref\("[a-zA-Z0-9._-]+", (true|false|-?[0-9]+|"([^\"]|\\")*")\);)|$)' palemoon/user.js
+
+echo "\033[37;1;41m Thunderbird \033[0m"
+grep -E -n -v '^[ 	]*(//|#[a-z]+|(user_pref\("[a-zA-Z0-9._-]+", (true|false|-?[0-9]+|"([^\"]|\\")*")\);)|$)' thunderbird/user.js
+
+echo "\033[37;1;41m Waterfox \033[0m"
+grep -E -n -v '^[ 	]*(//|#[a-z]+|(user_pref\("[a-zA-Z0-9._-]+", (true|false|-?[0-9]+|"([^\"]|\\")*")\);)|$)' waterfox/user.js
