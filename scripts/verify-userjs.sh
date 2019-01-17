3e8741163b47c951c9a476873633e9b1c55ad75e
bogachenko <bogachenkove@gmail.com>
2019-01-17 13:06:31 +0000

    Update verify-userjs.sh

--- scripts/verify-userjs.sh
+++ scripts/verify-userjs.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+## This file is part of the Liberty fox project,
+## Copyright (c) 2019 Bogachenko Vyacheslav
+##
+## Liberty fox is a free project: you can distribute it and/or modify
+## it in accordance with the MIT license published by the Massachusetts Institute of Technology.
+##
+## The Liberty fox project is distributed in the hope that it will be useful,
+## and is provided "AS IS", WITHOUT ANY WARRANTY, EXPRESSLY EXPRESSED OR IMPLIED.
+## WE ARE NOT RESPONSIBLE FOR ANY DAMAGES DUE TO THE USE OF THIS PROJECT OR ITS PARTS.
+## For more information, see the MIT license.
+##
+## Author: Bogachenko Vyacheslav <https://github.com/bogachenko>
+## Email: bogachenkove@gmail.com
+## Github: https://github.com/bogachenko/libertyfox/
+## Last modified: January 18, 2019
+## License: MIT <https://github.com/bogachenko/libertyfox/blob/master/LICENSE.md>
+## Problem reports: https://github.com/bogachenko/libertyfox/issues
+## Title: verify-userjs.sh
+## URL: https://raw.githubusercontent.com/bogachenko/libertyfox/master/scripts/verify-userjs.sh
+## Version: 0.0.2
+##
+## Download the entire Liberty fox project at https://github.com/bogachenko/libertyfox/archive/master.zip
+
 clear
 
 echo "\033[37;1;41m User.js \033[0m"
