8cd649b390da7ced0cdda8ce5c9eb11d9d5c9f02
grave4save <44759886+grave4save@users.noreply.github.com>
2019-01-26 05:08:38 +0000

    Update user.js

--- user.js
+++ user.js
@@ -1,26 +1,3 @@
-// This file is part of the Free Fox project,
-// Copyright (c) 2019 Bogachenko Vyacheslav
-//
-// Free Fox is a free project: you can distribute it and/or modify
-// it in accordance with the MIT license published by the Massachusetts Institute of Technology.
-//
-// The Free Fox project is distributed in the hope that it will be useful,
-// and is provided "AS IS", WITHOUT ANY WARRANTY, EXPRESSLY EXPRESSED OR IMPLIED.
-// WE ARE NOT RESPONSIBLE FOR ANY DAMAGES DUE TO THE USE OF THIS PROJECT OR ITS PARTS.
-// For more information, see the MIT license.
-//
-// Author: Bogachenko Vyacheslav <https://github.com/bogachenko>
-// Email: bogachenkove@gmail.com
-// Github: https://github.com/bogachenko/freefox/tree/master/firefox/
-// Last modified: January 24, 2019
-// License: MIT <https://github.com/bogachenko/freefox/blob/master/LICENSE.md>
-// Problem reports: https://github.com/bogachenko/freefox/issues
-// Title: user.js
-// URL: https://github.com/bogachenko/freefox/blob/master/firefox/user.js
-// Version: 64.0.2
-//
-// Download the entire Free Fox project at https://github.com/bogachenko/freefox/archive/master.zip
-
 // Warning in "about:config"
 // Предупреждение в "about:config"
 user_pref("general.warnOnAboutConfig", false);
